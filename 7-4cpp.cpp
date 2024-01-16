14e39dfe3ae4d7342e6a5a1b0be81809277c6cdc
sepehrghalamchi <151942721+sepehrghalamchi@users.noreply.github.com>
2024-01-16 07:10:33 +0330

    Create 7-4cpp.cpp

---
+++ 7-4cpp.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main(){
+	int m;
+	int n;
+	int i;
+	int gcd;
+	cin>>m;cin>>n;
+    while(m==n){
+    	gcd=n;
+    	break;
+	}
+	if (m==0)
+	{
+		cout<<n;
+	}
+    if(n==0)
+	{
+		cout<<m;
+	}
+if(m>n){
+	
+	for(i=n;i>0;i--){
+		if(m%i==0 && n%i==0){
+			 gcd= i;
+			break;
+		}
+		cout<<gcd;
+	}
+    }
+ if(n>m)
+{
+	for(i=m;i>0;i--){
+	if(m%i==0 && n%i==0){
+	     gcd= i;
+		break;
+	}	
+	}
+
+}
+cout<<gcd;
+}
